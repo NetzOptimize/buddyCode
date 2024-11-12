8675c605c48239a2aa77a19a47d8c7549f39145d
Varun NetzOptimize <113968429+varun606netzoptimize@users.noreply.github.com>
2024-11-12 18:29:58 +0530

    IOS name change to buddypassIOS to buddyPass

--- ios/Buddypass/AppDelegate.mm
+++ ios/Buddypass/AppDelegate.mm
@@ -13,7 +13,7 @@
   
       
   
-  self.moduleName = @"BuddypassIOS";
+  self.moduleName = @"Buddypass";
   // You can add your custom initial props in the dictionary below.
   // They will be passed down to the ViewController used by React Native.
   self.initialProps = @{};
